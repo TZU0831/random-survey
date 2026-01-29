8f59cba58ea3d7ac0d1f54d3e5ffa6088b596ba2
TZU0831 <131177477+TZU0831@users.noreply.github.com>
2026-01-29 13:24:27 +0800

    Update url.js

--- url.js
+++ url.js
@@ -8,6 +8,6 @@
 
 
 const urls = [
-	'https://forms.gle/7TxBsVzyFA4qsqo88',
-	'https://forms.gle/RNgDYWw1PybbP9Nn6'
+	'https://www.surveycake.com/s/2LNA3',
+	'https://www.surveycake.com/s/N7OGo'
 ];
